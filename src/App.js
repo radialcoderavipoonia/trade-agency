e6530c5b201aad9ff87370e21814251f4e83f72b
Bijender-137 <bijender.k1996@gmail.com>
2023-08-03 17:00:17 +0530

    accept

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import GameHosting from './components/GameHosting';
+import OurLocations from './components/OurLocations';
+import { Hero } from "./components/Hero";
 
 function App() {
   return (
     <>
-    
+      <GameHosting />
+      <OurLocations/>
+      <Hero />
     </>
   );
 }
