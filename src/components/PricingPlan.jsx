f4c5a467fb696a131a4b7c7d02bafb3789eb1cc1
radialcoderavipoonia <radialcoderavipoonia111@gmail.com>
2023-08-03 14:36:52 +0530

    new repo

---
+++ src/components/PricingPlan.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const PricingPlan = () => {
+  return (
+    <>
+      <section className="">
+        <div className="custom_container container">
+          <h2 className="ff_rubik fw-bold fs_4xl pt_43 mb-0 text-center color_whitefc">
+            Awesome <span className="color_blueod">Pricing Plan</span>
+          </h2>
+          <p className="ff_rubik fw-normal fs_1x8l color_whitefc text-center mb-0 pb_43 mw_647 me-auto ms-auto pt-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lacus
+            facilisi donec egestas egestas pellentesque magna.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+export default PricingPlan;
