a950d8028565934e0ed48438a823252ab460048c
Bijender-137 <bijender.k1996@gmail.com>
2023-08-04 09:27:17 +0530

    code push

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import PricingPlan from './components/PricingPlan';
+// import PricingPlan from './components/PricingPlan';
 // import MyNav from './components/MyNav'
 import GameHosting from './components/GameHosting';
 import OurLocations from './components/OurLocations';
-import { Hero } from "./components/Hero";
+// import { Hero } from "./components/Hero";
 
 function App() {
   return (
     <>
       {/* <MyNav /> */}
-      <PricingPlan/>
+      {/* <PricingPlan/> */}
       <GameHosting />
       <OurLocations/>
-      <Hero />
+      {/* <Hero /> */}
     </>
   );
 }
